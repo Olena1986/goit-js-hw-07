e92a461a702bcbf371a3c5e6b1093d2e20eee4d5
Olena1986 <125265067+Olena1986@users.noreply.github.com>
2023-04-18 18:20:35 +0200

    t

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
 const galleryList = document.querySelector('.gallery');
 
 const createGalleryItem = ({ preview, original, description }) => {
